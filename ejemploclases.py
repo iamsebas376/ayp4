fe057d7440bb38cfb5606dd97ed5791ff5d963d5
sebastian.arboleda <sebastian.arboleda@quipux.com>
2026-02-05 07:52:22 -0500

    Ejemplo de clases

---
+++ ejemploclases.py
@@ -0,0 +1,7 @@
+class estudiante: 
+    def __init__(self, documento, nombre, telefono):
+        self.documento = documento
+        self.nombre = nombre
+        self.telefono = telefono
+
+estudiante1 = estudiante(1001478559, "Sebastián", 3106011994)
